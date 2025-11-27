f1e0076ae8af89a71ce09843ba33b6b9fc3ce6ee
Amanuel Belete <amanuelbelete95@gmail.com>
2025-11-27 12:39:15 +0300

    Add event edit

---
+++ src/components/events/EditEvent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EventForm from './components/EventForm'
+
+function EditEvent() {
+  return (
+    <div>
+        <EventForm  />
+    </div>
+  )
+}
+
+export default EditEvent
